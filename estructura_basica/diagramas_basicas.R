73d34f3fa01ee57a23eb7064ed4c16c4e2c12a2d
snicasio <sergio.nicasio@gmail.com>
2020-03-08 22:33:38 -0600

    Actualización

--- estructura_basica/diagramas_basicas.R
+++ estructura_basica/diagramas_basicas.R
@@ -12,15 +12,34 @@ digraph boxes_and_circles {
         fontname = Helvetica]
   Vector; Atomico; Lista
 
+  # several 'edge' statements
+  Vector->Atomico Vector->Lista
+}
+")
+dgrm_vector
+
+
+
+dgrm_atomic <- grViz("
+digraph boxes_and_circles {
+
+  # a 'graph' statement
+  graph [overlap = true, fontsize = 10]
+
+  # several 'node' statements
+  node [shape = box,
+        width = 0.1,
+        fontname = Helvetica]
+  Atomico
+
   node [shape = circle,
         fixedsize = true,
-        width = 1] // sets as circles
+        width = 0.8] // sets as circles
   numeric; integer; double; logical; character
 
   # several 'edge' statements
-  Vector->Atomico Vector->Lista
   Atomico->numeric Atomico->logical Atomico->character
   numeric->integer numeric->double
 }
 ")
-dgrm_vector
+dgrm_atomic
