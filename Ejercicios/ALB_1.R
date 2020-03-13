dbe8757228a190b19ee4a0d94fdaedf240aee6fa
Alfredo <Alfredo@MacBook-Pro-de-Alfredo.local>
2020-03-12 21:19:38 -0600

    Alfredo Luna

--- Ejercicios/ALB_1.R
+++ Ejercicios/ALB_1.R
@@ -1,1 +1,36 @@
 #ALfredo Luna Bonilla
+#ALfredo Luna Bonilla    #   EJERCICIOS DE OPERADORES Y OBJETOS EN R
+
+##  Los datos empleados proceden de la pagina de FAOSTATS (http://www.fao.org/faostat/en/#data/QI/metadata)
+
+require(tibble)
+require(readr)
+
+FAO_df <- read.csv("FAO.csv")   #   data.frame
+FAO_tb <- read_csv("FAO.csv")   #   tibble
+
+typeof(FAO_df)
+typeof(FAO_tb)
+
+str(FAO_df)
+str(FAO_tb)
+
+typeof(FAO_tb$Area)
+aggregate(FAO_df$Value,list(FAO_df$Area),mean)
+aggregate(FAO_df$Value,list(FAO_df$Element),mean)
+
+
+MX1<-FAO_df[FAO_df$Area=="Mexico"&FAO_df$Year==2016,]
+MX2<-FAO_df[FAO_df$Area=="Mexico"|FAO_df$Year==2016,] ;MX2
+sum(MX1$Value)
+
+#formato fecha hora
+str(Sys.time())
+unclass(Sys.time())
+levels(FAO_df$Element)
+fecha_nac<-as.Date("1977-07-09");fecha_nac
+attributes(fecha_nac)
+attributes(FAO_df$Area)
+
+fechas<-FAO_df$Year;fechas
+fechas<-unique(fechas);fechas
