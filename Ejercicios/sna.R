4487b662e81e782aecb3afef1c5215b90261d7df
snicasio <sergio.nicasio@gmail.com>
2020-06-16 19:21:18 -0500

    Cambios efectuados por Sergio Nicasio

--- Ejercicios/sna.R
+++ Ejercicios/sna.R
@@ -13,20 +13,16 @@ typeof(FAO_df)
 
 str(FAO_df)
 #str(FAO_tb)
-<<<<<<< HEAD
-=======
 
 
 FAO_df
 
->>>>>>> b9a95bbb4d05ef4d29b87b5316bf68241072cb58
 FAO_df$Area
 FAO_df$Value
 
 names(FAO_df)
 
 FAO_df[ ,4]
-<<<<<<< HEAD
 
 FAO_df[ ,12]
 
@@ -55,7 +51,7 @@ unique(FAO_df$Area)
 tabla_cuatro <- FAO_df[FAO_df$Area=="Mexico"|FAO_df$Area=="United States of America"|FAO_df$Area=="Canada",c(4,12)]
 tabla_cuatro
 str(tabla_cuatro)
-=======
+
 FAO_df[ ,12]
 
 
@@ -91,13 +87,11 @@ tabla_cuatro <- FAO_df[FAO_df$Area == "Mexico" | FAO_df$Area == "United States o
 
 
 #FAO_df[FAO_df$Area == "Mexico" & FAO_df$Area == "United States of America" & FAO_df$Area == "Canada", c(4,12)]
->>>>>>> b9a95bbb4d05ef4d29b87b5316bf68241072cb58
 
 unique(FAO_df$Item)
 unique(FAO_df$Element)
 unique(FAO_df$Year)
 
-<<<<<<< HEAD
 tabla_cuatro
 
 #Ejercicio: elaboren una tabla que contenga los índices 
@@ -112,12 +106,10 @@ tabla_ejercicio <- FAO_df[FAO_df$Area=="Belize"| FAO_df$Area=="Guatemala"|
                     FAO_df$Item=="Livestock (PIN)"&
                     FAO_df$Year>=2000&FAO_df$Year<=2005,c(4,6,10) ]
 tabla_ejercicio
-=======
 
     #   Ejercicio: Elaboren una tabla que contenga los valores de los indices de produccion per capita neta por
     #               ganaderia en Centroamerica
     #               entre los periodos 2000-2005
->>>>>>> b9a95bbb4d05ef4d29b87b5316bf68241072cb58
 
 
 ejercicio_1 <- FAO_df[(FAO_df$Area == "Belize" |
@@ -137,6 +129,8 @@ ejercicio_1
         ##  SELECCION DE VARIABLES CON EL COMANDO "subset"
 
             ### Hacer un subset con la informacion de Mexico
+
+FAO_df <- read.csv("FAO.csv")
 
 names(FAO_df)
 
@@ -185,6 +179,131 @@ write.csv(agri_mc, "agri_sna.csv", row.names = F)
 aggregate(agri_mc$Value, by = list(agri_mc$Area), FUN = mean) # Paso 6
 
 
+        ##  OPERACIONES BASICAS
+
+numeros <- agri_mc$Value
+    numeros
+    str(numeros)
+
+            ### Sumas
+
+sum(numeros)   #   Comando "sum"
+1+2 #   Suma de valores unicos
+1 + numeros #   Suma de valor unico y vector numerico
+
+num_secuencia <- 1:10
+    num_secuencia
+
+suma_vectores <- num_secuencia + numeros #   Suma de vectores
+    suma_vectores
+
+num_secuencia <- num_secuencia+numeros
+    num_secuencia
+
+
+            ### Restas
+
+#   El ambiente de R carece de un comando que elabore restas
+
+1-2 #   Resta de valores unicos
+1 - numeros #   Reta de valor unico y vector numerico
+numeros - 1
+
+num_secuencia <- 1:10
+    num_secuencia
+
+resta_vectores <- num_secuencia - numeros #   Resta de vectores
+    resta_vectores
+
+numeros - num_secuencia
+
+num_secuencia <- num_secuencia-numeros
+    num_secuencia
+    num_secuencia[3]
+    num_secuencia[c(3,7,10)]
+    num_secuencia[c(10,3,7)]
+
+
+            ### Multiplicaciones y divisiones
+
+1*2
+1/2
+
+3*numeros
+1/numeros
+
+num_secuencia <- 1:10
+
+num_secuencia*numeros
+numeros*num_secuencia
+
+num_secuencia/numeros
+
+
+            ### Otros comandos de operaciones
+
+mean(numeros)   #   Media
+
+median(numeros) #   Mediana
+
+quantile(numeros)   #   Cuartiles
+
+range(numeros)  #   Rango
+
+var(numeros)    #   Varianza
+
+sd(numeros) #   Desviacion estandar
+
+
+
+        ##  EJERCICIO 3: Calcular los valores estandarizados de lo siguiente:
+        ##  1) Produccion neta agricola de Mexico (todos los anios)
+        ##  2) Produccion neta pecuaria de Mexico (todos los anios)
+
+
+#   Paso 1: Extraer la informacion de los indices de productividad para Mexico
+#   Paso 2: Extraer la informacion para los rubros de agricultura y ganaderia
+#   Paso 3: Calcular los valores estandarizados
+#   Paso 4: Representarlos en un histograma
+
+unique(FAO_df$Element)
+unique(FAO_df$Item)
+
+names(FAO_df)
+names(FAO_df)[c(6,8)]
+
+names(FAO_df)[c(6,8)] <- c("Indicador","Rubro")
+
+names(FAO_df)
+names(FAO_df)[c(6,8)]
+
+
+FAO_df <- read.csv("FAO.csv")
+
+
+
+agri_mx <- subset(FAO_df,
+                  subset = Area == "Mexico" &
+                      Item == "Agriculture (PIN)" &
+                      Element == "Net Production Index Number (2004-2006 = 100)",
+                  select = "Value")
+
+str(agri_mx)
+mean(agri_mx$Value)
+mean(agri_mx)
+
+agri_mx <- agri_mx$Value
+    agri_mx
+    str(agri_mx)
+
+mean(agri_mx)
+sd(agri_mx)
+
+
+z_agri <- (agri_mx-mean(agri_mx))/sd(agri_mx)
+
+hist(z_agri)  #   Histograma
+hist(z_agri, main = "Produccion neta agricola")
 
 
 
